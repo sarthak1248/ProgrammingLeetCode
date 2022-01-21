4bdec37a126c8f6de6917db867e7c7536df3325a
Sarthak Singhal <79219886+sarthak1248@users.noreply.github.com>
2022-01-21 10:41:25 +0530

    Time: 86 ms (32.90%), Space: 49.9 MB (45.28%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-    int start_if = 0;
-        int total_fuel_incircle = 0;
-        int least = 0;
-        for (int i = 0 ; i < gas.size(); i++) {
-            /*
-            total fuel to go around circle if it is less than zero we cannot start from any index
-            */
-            total_fuel_incircle += gas[i] - cost[i]; 
-            if (total_fuel_incircle < least) {
-                start_if = i+1; 
-                least = total_fuel_incircle ; 
+        int fuel=0;
+        int least=0;
+        int index=0;
+        for(int i=0;i<gas.size();i++){
+            fuel+= gas[i]-cost[i];
+            if(fuel<least){
+                
+                index=i+1;
+                least=fuel;
+              
             }
         }
-        return total_fuel_incircle >= 0 ? start_if % gas.size() : -1; 
-    }    
-    
+            
+            if(fuel>=0){
+                return index%gas.size();
+            }
+            
+        return -1;
+        
+    }
 };
