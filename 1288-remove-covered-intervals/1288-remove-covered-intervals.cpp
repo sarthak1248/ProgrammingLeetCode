9f7fea5b120177013e6b778b86bab47d6bdb8f93
Sarthak Singhal <79219886+sarthak1248@users.noreply.github.com>
2022-02-20 15:46:09 +0530

    Time: 29 ms (60.96%), Space: 11.3 MB (59.43%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
-    sort(intervals.begin(),intervals.end());
-        int x1= intervals[0][0];
-        int x2= intervals[0][1];
-        int res=1;
-       for(int i=1;i<intervals.size();i++){
-           if(intervals[i][0]>x1  && intervals[i][1]>x2){
-               res++;
-           }
-           
-           if(intervals[i][1]>x2){
-               x1=intervals[i][0];
-               x2=intervals[i][1];
-           }
-       }     
-        return res;
+     sort(intervals.begin(),intervals.end());
+        int left=-1;
+        int right=-1;
+        int ans=0;
+        
+        for(int i=0;i<intervals.size();i++){
+            if(intervals[i][0]>left && intervals[i][1]>right){
+                left = intervals[i][0];
+               
+                ans++;
+            }
+            right= max(right,intervals[i][1]);
+        }
+        return ans;
     }
 };
