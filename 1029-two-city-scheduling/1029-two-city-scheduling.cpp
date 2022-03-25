bc11acf18f2213ec7ee47b5091207b5d9d88cf1a
Sarthak Singhal <79219886+sarthak1248@users.noreply.github.com>
2022-03-25 13:48:42 +0530

    Time: 0 ms (100.00%), Space: 8 MB (47.20%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+     int costA=0;
+        vector<int> diff;
+        for(int i=0;i<costs.size();i++){
+            costA=costA+costs[i][0];
+            diff.push_back(costs[i][1]-costs[i][0]);
+        }
+        
+        sort(diff.begin(),diff.end());
+        for(int i=0;i<costs.size()/2;i++){
+            costA= costA+diff[i];
+        }
+        return costA;
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        
+        
+    }
+};
