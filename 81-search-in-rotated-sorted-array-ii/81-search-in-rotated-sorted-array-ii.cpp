3a26a6906033dab092cdcc33fde040912eeb87e5
Sarthak Singhal <79219886+sarthak1248@users.noreply.github.com>
2022-03-29 00:02:37 +0530

    Time: 14 ms (23.27%), Space: 13.9 MB (90.12%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
+        sort(nums.begin(),nums.end());
+        int low =0;
+        int high=nums.size()-1;
+        int mid = (high+low)/2;
+        while(low<=high){
+            mid=(low+high)/2;
+            if(nums[mid]==target){
                 return true;
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
             }
         }
         return false;
+        
     }
 };
